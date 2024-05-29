f4704a991c412d3cccaf23f9ed2531dd96784ca0
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-05-29 16:28:06 +0700

    chore: setup express

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Working");
+  res.send("Working guys");
 });
 
 app.listen(3000, () => {
