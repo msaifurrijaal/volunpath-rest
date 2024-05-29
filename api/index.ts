86d32e3a8c6573e8856c819c51ccf23f9f105654
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-05-29 16:54:00 +0700

    chore: update setup express

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src";
 
-export default app;
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
