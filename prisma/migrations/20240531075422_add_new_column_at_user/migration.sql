99689e674d2c997ea375178d1cf33c2e0091ff23
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-06-01 14:51:04 +0700

    chore: setup .env local and prod

--- prisma/migrations/20240531075422_add_new_column_at_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "events" ADD COLUMN     "image" TEXT;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "image" TEXT;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import express, { Request, Response } from "express";
 import rootRouter from "./routes";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+} else {
+  dotenv.config({ path: ".env.production" });
+}
 
 const app = express();
 app.use(express.json());
@@ -10,8 +17,8 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/", rootRouter);
 
-app.listen(7000, () => {
-  console.log(`App running on port ${7000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
 });
 
 export default app;
