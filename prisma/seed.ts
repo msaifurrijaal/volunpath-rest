7f47b7ba625008b8f5494d8e5f59ac6f714b6910
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-07-06 11:32:28 +0700

    update seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -78,7 +78,8 @@ async function main() {
       name: "Mitra Organization",
       address: "123 Mitra Street",
       focus: "Community Development",
-      description: "Mitra Organization focuses on community development projects.",
+      description:
+        "Mitra Organization focuses on community development projects.",
     },
   });
 
@@ -86,48 +87,99 @@ async function main() {
   await prisma.categoryEvent.createMany({
     data: [
       { name: "Education", description: "Events related to education." },
-      { name: "Health", description: "Events related to health." },
-      { name: "Environment", description: "Events related to environmental conservation." },
+      { name: "Healthy Food", description: "Events related to healthy food." },
+      {
+        name: "Medical Help",
+        description: "Events related to medical help.",
+      },
+      {
+        name: "Social Service",
+        description: "Events related to social service.",
+      },
     ],
   });
 
-  // Fetch the category IDs
   const educationCategory = await prisma.categoryEvent.findUnique({
     where: { name: "Education" },
   });
 
   const healthCategory = await prisma.categoryEvent.findUnique({
-    where: { name: "Health" },
+    where: { name: "Medical Help" },
   });
 
-  // Create Events
+  const socialCategory = await prisma.categoryEvent.findUnique({
+    where: { name: "Social Service" },
+  });
+
+  const foodCategory = await prisma.categoryEvent.findUnique({
+    where: { name: "Healthy Food" },
+  });
+
   const event1 = await prisma.event.create({
     data: {
       organizer_id: organizationUser.id,
-      title: "Community Clean-Up",
-      description: "Join us in cleaning up the local park.",
+      title: "Belajar Bersama Anak-anak Panti Asuhan Bunga",
+      description:
+        "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
       date: new Date("2024-06-01T10:00:00Z"),
-      location: "Central Park",
+      location: "Batu, Malang",
       slots_needed: 10,
       slots_available: 10,
       category_id: educationCategory?.id,
       status: "open_registration",
-      image: "https://res.cloudinary.com/df5zedkiz/image/upload/v1717301562/ogjfetkcpj0vfgcmqppc.jpg",
+      image:
+        "https://res.cloudinary.com/df5zedkiz/image/upload/v1717301562/ogjfetkcpj0vfgcmqppc.jpg",
     },
   });
 
   const event2 = await prisma.event.create({
     data: {
       organizer_id: organizationUser.id,
-      title: "Health Camp",
-      description: "Providing free health check-ups for the community.",
+      title: "Cek Kesehatan Anak Sekolah",
+      description:
+        "Providing free health check-ups for the community. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
       date: new Date("2024-06-15T09:00:00Z"),
-      location: "Community Center",
+      location: "Malang",
       slots_needed: 5,
       slots_available: 5,
       category_id: healthCategory?.id,
       status: "open_registration",
-      image: "https://res.cloudinary.com/df5zedkiz/image/upload/v1717299213/ovhkjb3natpbsoyh3z0c.jpg",
+      image:
+        "https://res.cloudinary.com/df5zedkiz/image/upload/v1717299213/ovhkjb3natpbsoyh3z0c.jpg",
+    },
+  });
+
+  const event3 = await prisma.event.create({
+    data: {
+      organizer_id: organizationUser.id,
+      title: "Berbagi Makanan Sehat untuk Siswa SDN Maju Indonesia",
+      description:
+        "Membantu siswa SDN Maju Indonesia menyediakan makanan sehat. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+      date: new Date("2024-06-01T10:00:00Z"),
+      location: "Lowokwaru, Malang",
+      slots_needed: 10,
+      slots_available: 10,
+      category_id: foodCategory?.id,
+      status: "open_registration",
+      image:
+        "https://res.cloudinary.com/df5zedkiz/image/upload/v1720239783/food_hxo42f.jpg",
+    },
+  });
+
+  const event4 = await prisma.event.create({
+    data: {
+      organizer_id: organizationUser.id,
+      title: "Kerja Bakti bersama warga Kepanjen",
+      description:
+        "Membantu warga Kepanjen Kerja Bakti. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      date: new Date("2024-06-01T10:00:00Z"),
+      location: "Central Park",
+      slots_needed: 10,
+      slots_available: 10,
+      category_id: socialCategory?.id,
+      status: "open_registration",
+      image:
+        "https://res.cloudinary.com/df5zedkiz/image/upload/v1717301562/ogjfetkcpj0vfgcmqppc.jpg",
     },
   });
 
